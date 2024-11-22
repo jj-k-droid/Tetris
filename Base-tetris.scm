dbede7e33d1420ed54e12c4beed7a0cb9aa6e4ed
jojo <93551276+jordantheblob@users.noreply.github.com>
2024-11-22 10:54:53 -0600

    updated Base-tetris.scm

--- Base-tetris.scm
+++ Base-tetris.scm
@@ -1,109 +1,12 @@
 ; tetrominoes (shapes)
 
-(import image)
-(import canvas)
-(import html)
-
-(struct tetromino
-  (x y rotations))
-
-;;; (block color) -> image?
-;;;    color : color?
-;;; Returns an image of a block with the given color
-(define block
-  (lambda (color)
-    (overlay (solid-square 18 color)
-             (solid-square 20 (rgb-darker (rgb-darker color))))))
-
-(define I
-  (let* ([color (rgb 1 230 254)]
-         [flat (beside (block color) (block color) (block color) (block color))]
-         [i (above (block color) (block color) (block color) (block color))])
-  (tetromino 0 0 (vector flat i flat i))))
-
-(tetromino-rotations I)
-
-(define J
-  (let* ([color (rgb 24 1 255)]
-         [flat (above/align "left" (block color)
-                            (beside (block color) (block color) (block color)))]
-         [filpped-j (beside/align "top" 
-                                      (above (block color) (block color) (block color))
-                                      (block color))]
-         [filpped-flat (above/align "right" (beside (block color) (block color) (block color))
-                                        (block color))]
-         [j (beside/align "bottom" (block color)
-                          (above (block color) (block color) (block color)))])
-  (tetromino 0 0 (vector flat filpped-j filpped-flat j))))
-
-(tetromino-rotations J)
-
-(define L
-  (let* ([color (rgb 255 115 9)]
-         [flat (above/align "right" (block color)
-                            (beside (block color) (block color) (block color)))]
-         [l (beside/align "bottom" 
-                          (above (block color) (block color) (block color))
-                          (block color))]
-         [filpped-flat (above/align "left" (beside (block color) (block color) (block color) )
-                                        (block color))]
-         [filpped-l (beside/align "top" (block color)
-                                       (above (block color) (block color) (block color)))])
-  (tetromino 0 0 (vector flat l filpped-flat filpped-l))))
-
-(tetromino-rotations L)
-
-(define O
-  (let* ([color (rgb 255 222 2)]
-         [o (above (beside (block color) (block color))
-                   (beside (block color) (block color)))])
-        (tetromino 0 0 (vector o o o o))))
-
-(tetromino-rotations O)
-
-(define S
-  (let* ([color (rgb 102 253 3)]
-         [s (above (beside (block (rgb 0 0 0 0)) (block color) (block color))
-                   (beside (block color) (block color) (block (rgb 0 0 0 0))))]
-         [vertical-s (beside (above (block color) (block color) (block (rgb 0 0 0 0)))
-                             (above (block (rgb 0 0 0 0)) (block color) (block color)))])
-        (tetromino 0 0 (vector s vertical-s s vertical-s))))
-
-(tetromino-rotations S)
-
-(define Z
-  (let* ([color (rgb 254 17 60)]
-         [z (above (beside (block color) (block color) (block (rgb 0 0 0 0)))
-                   (beside (block (rgb 0 0 0 0)) (block color) (block color)))]
-         [vertical-z (beside (above (block (rgb 0 0 0 0)) (block color) (block color))
-                             (above (block color) (block color) (block (rgb 0 0 0 0))))])
-        (tetromino 0 0 (vector z vertical-z z vertical-z))))
-
-(tetromino-rotations Z)
-
-(define T
-  (let* ([color (rgb 184 3 253)]
-         [flat (above/align "middle" (block color)
-                            (beside (block color) (block color) (block color)))]
-         [vertical-1 (beside/align "center" (above (block color) (block color) (block color))
-                                   (block color))]
-         [flipped-flat (above/align "middle" 
-                                    (beside (block color) (block color) (block color))
-                                    (block color))]
-         [vertical-2 (beside/align "center" (block color)
-                                   (above (block color) (block color) (block color)))])
-         (tetromino 0 0 (vector flat vertical-1 flipped-flat vertical-2))))
-
-(tetromino-rotations T)
-
-
-
-; tetris grid
+;tetris grid
 (import image)
 (import canvas)
 (import lab)
+(import html)
 (define colors (list
-                (rgb 0 0 0)
+                 (rgb 0 0 0)
                  (rgb 255 0 0)
                  (rgb 255 127 0)
                  (rgb 255 255 0)
@@ -112,30 +15,16 @@
                  (rgb 0 0 255)
                  (rgb 255 0 255)))
 
-;Grid Functions
-(struct grid (width height contents))
+(struct tetromino
+  (x y rotations))
 
-(define create-grid
-  (lambda (width height)
-    (grid width height (make-vector (* width height) 0))))
-
-(define grid-ref 
-  (lambda (grid x y)
-    (vector-ref (grid-contents grid) (+ x (* y (grid-width grid))))))
-
-(define grid-set!
-  (lambda (grid x y value)
-    (vector-set! (grid-contents grid) (+ x (* y (grid-width grid))) value )))
-
-(define get-row-helper 
-  (lambda (grid y i)
-    (if (>= i (grid-width grid))
-        null
-        (cons (grid-ref grid i y) (get-row-helper grid y (+ i 1))))))
-
-(define get-row
-  (lambda (grid y)
-    (get-row-helper grid y 0)))
+;;; (tetris-square 20 color) -> image?
+;;;    color : color?
+;;; Returns an image of a block with the given color
+(define block
+  (lambda (color)
+    (overlay (solid-square 18 color)
+             (solid-square 20 (rgb-darker (rgb-darker color))))))
 
 ;Drawing functions
 (define shadow 
@@ -161,6 +50,116 @@
     (overlay (solid-square (* size 0.75) color)
              (path size size (list (pair 0 0) (pair 0 size) (pair size 0)) "solid" highlight)
              (solid-square size shadow)))))
+
+
+; shapes
+(part "shapes")
+(define I
+  (let* ([color (rgb 1 230 254)]
+         [flat (beside (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color))]
+         [i (above (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color))])
+  (tetromino 0 0 (vector flat i flat i))))
+
+(tetromino-rotations I)
+
+(define J
+  (let* ([color (rgb 24 1 255)]
+         [flat (above/align "left" (tetris-square 20 color)
+                            (beside (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color)))]
+         [filpped-j (beside/align "top" 
+                                      (above (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color))
+                                      (tetris-square 20 color))]
+         [filpped-flat (above/align "right" (beside (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color))
+                                        (tetris-square 20 color))]
+         [j (beside/align "bottom" (tetris-square 20 color)
+                          (above (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color)))])
+  (tetromino 0 0 (vector flat filpped-j filpped-flat j))))
+
+(tetromino-rotations J)
+
+(define L
+  (let* ([color (rgb 255 115 9)]
+         [flat (above/align "right" (tetris-square 20 color)
+                            (beside (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color)))]
+         [l (beside/align "bottom" 
+                          (above (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color))
+                          (tetris-square 20 color))]
+         [filpped-flat (above/align "left" (beside (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color) )
+                                        (tetris-square 20 color))]
+         [filpped-l (beside/align "top" (tetris-square 20 color)
+                                       (above (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color)))])
+  (tetromino 0 0 (vector flat l filpped-flat filpped-l))))
+
+(tetromino-rotations L)
+
+(define O
+  (let* ([color (rgb 255 222 2)]
+         [o (above (beside (tetris-square 20 color) (tetris-square 20 color))
+                   (beside (tetris-square 20 color) (tetris-square 20 color)))])
+        (tetromino 0 0 (vector o o o o))))
+
+(tetromino-rotations O)
+
+(define S
+  (let* ([color (rgb 102 253 3)]
+         [s (above (beside (block (rgb 0 0 0 0)) (tetris-square 20 color) (tetris-square 20 color))
+                   (beside (tetris-square 20 color) (tetris-square 20 color) (block (rgb 0 0 0 0))))]
+         [vertical-s (beside (above (tetris-square 20 color) (tetris-square 20 color) (block (rgb 0 0 0 0)))
+                             (above (block (rgb 0 0 0 0)) (tetris-square 20 color) (tetris-square 20 color)))])
+        (tetromino 0 0 (vector s vertical-s s vertical-s))))
+
+(tetromino-rotations S)
+
+(define Z
+  (let* ([color (rgb 254 17 60)]
+         [z (above (beside (tetris-square 20 color) (tetris-square 20 color) (block (rgb 0 0 0 0)))
+                   (beside (block (rgb 0 0 0 0)) (tetris-square 20 color) (tetris-square 20 color)))]
+         [vertical-z (beside (above (block (rgb 0 0 0 0)) (tetris-square 20 color) (tetris-square 20 color))
+                             (above (tetris-square 20 color) (tetris-square 20 color) (block (rgb 0 0 0 0))))])
+        (tetromino 0 0 (vector z vertical-z z vertical-z))))
+
+(tetromino-rotations Z)
+
+(define T
+  (let* ([color (rgb 184 3 253)]
+         [flat (above/align "middle" (tetris-square 20 color)
+                            (beside (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color)))]
+         [vertical-1 (beside/align "center" (above (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color))
+                                   (tetris-square 20 color))]
+         [flipped-flat (above/align "middle" 
+                                    (beside (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color))
+                                    (tetris-square 20 color))]
+         [vertical-2 (beside/align "center" (tetris-square 20 color)
+                                   (above (tetris-square 20 color) (tetris-square 20 color) (tetris-square 20 color)))])
+         (tetromino 0 0 (vector flat vertical-1 flipped-flat vertical-2))))
+
+(tetromino-rotations T)
+
+;Grid Functions
+(part "grid")
+(struct grid (width height contents))
+
+(define create-grid
+  (lambda (width height)
+    (grid width height (make-vector (* width height) 0))))
+
+(define grid-ref 
+  (lambda (grid x y)
+    (vector-ref (grid-contents grid) (+ x (* y (grid-width grid))))))
+
+(define grid-set!
+  (lambda (grid x y value)
+    (vector-set! (grid-contents grid) (+ x (* y (grid-width grid))) value )))
+
+(define get-row-helper 
+  (lambda (grid y i)
+    (if (>= i (grid-width grid))
+        null
+        (cons (grid-ref grid i y) (get-row-helper grid y (+ i 1))))))
+
+(define get-row
+  (lambda (grid y)
+    (get-row-helper grid y 0)))
 
 
 ;functions to convert grid to a drawing
@@ -222,9 +221,4 @@
 ;display the canvas
 (problem "click the canvas to edit the pixels \n    |\n    v")
 canv
-
-
-
-
-
 
